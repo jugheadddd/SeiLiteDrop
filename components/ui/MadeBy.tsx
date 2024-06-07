2489b73f0109286504c3d7af7aa1fae7f87c536c
jughead <155861122+jugheadddd@users.noreply.github.com>
2024-06-07 17:29:52 -0400

    Updated credit

--- components/ui/MadeBy.tsx
+++ components/ui/MadeBy.tsx
@@ -7,7 +7,7 @@ const MadeBy: React.FC = () => {
   return (
     <div style={containerStyle}>
       <p style={textStyle}>Originally by</p>
-      <h1 className="text-5xl font-alarm uppercase text-markPink-900">
+      <h1 className="text-4xl font-alarm uppercase text-markPink-900">
         <Link href="https://www.gaslite.org/">Gaslite Drop</Link>
       </h1>
     </div>
@@ -29,7 +29,7 @@ const containerStyle: React.CSSProperties = {
 
 const textStyle: React.CSSProperties = {
     margin: '0 10px 0 0',
-    fontSize: '22px',
+    fontSize: '20px',
     fontWeight: 'bold',
     color: '#EA33F6', 
 };
